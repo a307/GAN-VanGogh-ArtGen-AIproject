ec5b6c917f7294597f84550a7b1a20beb7169d57
SpartanI117 <56614496+IsaiahB312@users.noreply.github.com>
2023-04-16 21:48:04 -0700

    Changed the code of the Generator and Discriminator and added some comments as well.

--- Discriminator.py
+++ Discriminator.py
@@ -1,23 +1,75 @@
+# import torch.nn as nn
+# class Discriminator(nn.Module):
+#     def __init__(self):
+#         super(Discriminator, self).__init__()
+#
+#         self.model = nn.Sequential(
+#             nn.Linear(3 * 256 * 256, 1024),
+#             nn.LeakyReLU(0.2),
+#             nn.Dropout(0.3),
+#             nn.Linear(1024, 512),
+#             nn.LeakyReLU(0.2),
+#             nn.Dropout(0.3),
+#             nn.Linear(512, 256),
+#             nn.LeakyReLU(0.2),
+#             nn.Dropout(0.3),
+#             nn.Linear(256, 1),
+#             nn.Sigmoid()
+#         )
+#
+#     def forward(self, img):
+#         img = img.view(img.size(0), -1)
+#         validity = self.model(img)
+#         return validity
+
+# New code here
 import torch.nn as nn
+
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-        self.model = nn.Sequential(
-            nn.Linear(3 * 64 * 64, 1024),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(0.3),
-            nn.Linear(1024, 512),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(0.3),
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(0.3),
-            nn.Linear(256, 1),
-            nn.Sigmoid()
-        )
+        # Define the first linear layer, activation function, and dropout layer
+        self.layer1 = nn.Linear(3 * 256 * 256, 1024) # Change the 256 * 256 here for a different size
+        self.activation1 = nn.LeakyReLU(0.2)
+        self.dropout1 = nn.Dropout(0.3)
+
+        # Define the second linear layer, activation function, and dropout layer
+        self.layer2 = nn.Linear(1024, 512)
+        self.activation2 = nn.LeakyReLU(0.2)
+        self.dropout2 = nn.Dropout(0.3)
+
+        # Define the third linear layer, activation function, and dropout layer
+        self.layer3 = nn.Linear(512, 256)
+        self.activation3 = nn.LeakyReLU(0.2)
+        self.dropout3 = nn.Dropout(0.3)
+
+        # Define the fourth linear layer and its activation function (output layer)
+        self.layer4 = nn.Linear(256, 1)
+        self.activation4 = nn.Sigmoid()
 
     def forward(self, img):
+        # Reshape the input image to a 1D tensor
         img = img.view(img.size(0), -1)
-        validity = self.model(img)
+
+        # Pass the image through the first layer, activation function, and dropout layer
+        img = self.layer1(img)
+        img = self.activation1(img)
+        img = self.dropout1(img)
+
+        # Pass the image through the second layer, activation function, and dropout layer
+        img = self.layer2(img)
+        img = self.activation2(img)
+        img = self.dropout2(img)
+
+        # Pass the image through the third layer, activation function, and dropout layer
+        img = self.layer3(img)
+        img = self.activation3(img)
+        img = self.dropout3(img)
+
+        # Pass the image through the fourth (output) layer and its activation function
+        img = self.layer4(img)
+        validity = self.activation4(img)
+
+        # Return the validity score (probability of the image being real)
         return validity
--- Generator.py
+++ Generator.py
@@ -2,22 +2,66 @@ import torch
 import torch.nn as nn
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
-        self.model = nn.Sequential(
-            nn.Linear(100, 256),
-            nn.LeakyReLU(0.2),
-            nn.Linear(256, 512),
-            nn.LeakyReLU(0.2),
-            nn.Linear(512, 1024),
-            nn.LeakyReLU(0.2),
-            nn.Linear(1024, 3 * 64 * 64),
-            nn.Tanh()
-        )
+        # Define the layers
+        self.fc1 = nn.Linear(100, 256)
+        self.fc2 = nn.Linear(256, 512)
+        self.fc3 = nn.Linear(512, 1024)
+        self.fc4 = nn.Linear(1024, 3 * 256 * 256) # Change the 256 * 256 for a different size
+
+        # Define the activation functions
+        self.leaky_relu = nn.LeakyReLU(0.2)
+        self.tanh = nn.Tanh()
 
     def forward(self, z):
-        img = self.model(z).to(device)
-        img = img.view(img.size(0), 3, 64, 64)
-        return img
+        # Apply the first layer and activation
+        z = self.fc1(z)
+        z = self.leaky_relu(z)
+
+        # Apply the second layer and activation
+        z = self.fc2(z)
+        z = self.leaky_relu(z)
+
+        # Apply the third layer and activation
+        z = self.fc3(z)
+        z = self.leaky_relu(z)
+
+        # Apply the fourth layer and activation
+        z = self.fc4(z)
+        img = self.tanh(z)
+
+        # Reshape the output to the desired image size
+        img = img.view(img.size(0), 3, 256, 256) # Change the 256, 256 here for a different size
+
+        return img.to(device)
+
+# Old Code
+# import torch
+# import torch.nn as nn
+#
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# class Generator(nn.Module):
+#     def __init__(self):
+#         super(Generator, self).__init__()
+#
+#         self.model = nn.Sequential(
+#             nn.Linear(100, 256),
+#             nn.LeakyReLU(0.2),
+#             nn.Linear(256, 512),
+#             nn.LeakyReLU(0.2),
+#             nn.Linear(512, 1024),
+#             nn.LeakyReLU(0.2),
+#             nn.Linear(1024, 3 * 256 * 256),
+#             nn.Tanh()
+#         )
+#
+#     def forward(self, z):
+#         img = self.model(z).to(device)
+#         img = img.view(img.size(0), 3, 256, 256)
+#         return img
+
+
